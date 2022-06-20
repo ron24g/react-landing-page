e903d8d3b64928d9a55244031180155d2522caca
ron24g <ron24g@yahoo.com>
2022-06-20 03:13:39 +0000

    must finish layout

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const JumboTron = () => {
+	return (
+		<div className="container-fluid py-5">
+			<h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+			<p className="col-md-8 fs-4">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+				laoreet tristique suscipit. Aliquam vitae turpis eu dui viverra
+				bibendum. Mauris condimentum aliquam porta. Class aptent taciti
+				sociosqu ad litora torquent per conubia nostra, per inceptos
+				himenaeos
+			</p>
+			<button className="btn btn-primary btn-lg" type="button">
+				Call to action!
+			</button>
+		</div>
+	);
+};
+
+export default JumboTron;
